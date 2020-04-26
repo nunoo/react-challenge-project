1a64771d18389fce91da501c515e084775a44106
nunoo <nunoo@nunoo.local>
2020-04-26 16:39:18 -0700

    fix(vieworders): fixed time format

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
+
 import './viewOrders.css';
 
-
 class ViewOrders extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            orders: []
-        }
-        
+    state = {
+        orders: []
     }
 
-    // getter - gets all 
     componentDidMount() {
         fetch(`${SERVER_IP}/api/current-orders`)
             .then(response => response.json())
             .then(response => {
-                console.log("orders", response.orders);
                 if(response.success) {
                     this.setState({ orders: response.orders });
                 } else {
@@ -27,33 +21,16 @@ class ViewOrders extends Component {
             });
     }
 
-    // add in your functions Edit and Delete 
-    editOrder() {
-
-    }
-
-    deleteOrder(order) {
-        fetch(`${SERVER_IP}/api/delete-order`, {
-            method: 'POST',
-            body: JSON.stringify({
-                id: order._id
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(res => res.json())
-        .then(response => console.log(response.success))
-        .catch(error => console.error(error));
-        
-    }
-
     render() {
         return (
             <Template>
                 <div className="container-fluid">
                     {this.state.orders.map(order => {
-                        const createdDate = new Date(order.createdAt);
+                        let options = {hour:'numeric', minute: 'numeric', second: 'numeric',
+                                       hour12: false
+                                     }           
+                        const createdDate = new Intl.DateTimeFormat('en-US', options).format(new Date(order.createdAt));
+
                         return (
                             <div className="row view-order-container" key={order._id}>
                                 <div className="col-md-4 view-order-left-col p-3">
@@ -61,12 +38,12 @@ class ViewOrders extends Component {
                                     <p>Ordered by: {order.ordered_by || ''}</p>
                                 </div>
                                 <div className="col-md-4 d-flex view-order-middle-col">
-                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                                    <p>Order placed at {`${createdDate}`}</p>
                                     <p>Quantity: {order.quantity}</p>
                                  </div>
                                  <div className="col-md-4 view-order-right-col">
                                      <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger" onClick={() => this.deleteOrder(order)}>Delete</button>
+                                     <button className="btn btn-danger">Delete</button>
                                  </div>
                             </div>
                         );
